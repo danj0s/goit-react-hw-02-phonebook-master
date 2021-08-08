c0be83f7c38dc5f04af8a9b06f6d7c8633c80084
danj0s <danja2001@mail.ua>
2021-08-09 00:13:45 +0300

    done

---
+++ src/components/Form/Input/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export class Input extends Component {
+  render() {
+    const { name, changeInputForFilter, changeInput } = this.props;
+    const arr = [...name];
+    arr.splice(0, 1, arr[0].toUpperCase());
+    return (
+      <>
+        <label htmlFor={name}>{arr}</label>
+        <input
+          onChange={name === "filter" ? changeInputForFilter : changeInput}
+          type="text"
+          id={name}
+        />
+      </>
+    );
+  }
+}
+
+export default Input;
